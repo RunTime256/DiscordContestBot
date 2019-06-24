80ab2f860ab1f62c185697338de6a49f050ad0ed
Nathan-Webb <nathanwebb7550@gmail.com>
2019-06-24 14:09:48 -0500

    completed submission collection

--- src/main/java/me/spider/contestbot/structures/SubmissionCollection.java
+++ src/main/java/me/spider/contestbot/structures/SubmissionCollection.java
@@ -5,26 +5,48 @@ import java.util.ArrayList;
 public class SubmissionCollection {
     private ArrayList<Submission> submissions = new ArrayList<>();
     private String contestID;
-    public SubmissionCollection(String contestID){
+
+    public SubmissionCollection(String contestID) {
         this.contestID = contestID;
     }
-    public SubmissionCollection(String contestID, ArrayList<Submission> submissions){
+
+    public SubmissionCollection(String contestID, ArrayList<Submission> submissions) {
         this.submissions = submissions;
         this.contestID = contestID;
     }
 
-    public boolean addSubmission(Submission submission){
+    public boolean addSubmission(Submission submission) {
         return submissions.add(submission);
     }
 
-    public boolean removeSubmission(Submission submission){
+    public boolean removeSubmission(Submission submission) {
         return submissions.remove(submission);
     }
 
-    public boolean removeSubmission(String submission){
-        submissions.forEach( sub -> {
-
+    public void removeSubmission(String mysteryID) {
+        submissions.forEach(sub -> {
+            if (sub.getSubmitterID().equals(mysteryID) || sub.getSubmissionID().equals(mysteryID)) {
+                submissions.remove(sub);
+            }
         });
+    }
+
+    public boolean hasSubmitted(String userID) {
+        for (Submission sub : submissions) {
+            if (sub.getSubmitterID().equals(userID)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public int countSubmissions(String userID){
+        int amount = 0;
+        for(Submission sub : submissions){
+            if(sub.getSubmitterID().equals(userID)){
+                amount++;
+            }
+        }
+        return amount;
     }
 }
