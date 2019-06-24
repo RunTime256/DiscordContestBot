8f4e918a87f4019daee59b28954194ab257c9595
Nathan-Webb <nathanwebb7550@gmail.com>
2019-06-24 16:14:16 -0500

    created period

---
+++ src/main/java/me/spider/contestbot/structures/Period.java
@@ -0,0 +1,34 @@
+package me.spider.contestbot.structures;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Calendar;
+
+public @Data class Period {
+    private @Getter long startTime;
+    private @Getter long endTime;
+
+    public Period(long startTime, long endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Period(long endTime){
+        this.startTime = Calendar.getInstance().getTimeInMillis();
+        this.endTime = endTime;
+    }
+
+    public boolean hasEnded(){
+        return endTime <= Calendar.getInstance().getTimeInMillis();
+    }
+
+    public boolean isInPeriod(){
+        long now = Calendar.getInstance().getTimeInMillis();
+        return  now >= startTime && now <= endTime;
+    }
+
+    public boolean isInPeriod(long epoch){
+        return  epoch >= startTime && epoch <= endTime;
+    }
+}
