10457fe91a87169578085c033b31fcf360aee181
Nathan-Webb <nathanwebb7550@gmail.com>
2019-06-24 13:09:54 -0500

    added submission with lombok

--- src/main/java/me/spider/contestbot/structures/Submission.java
+++ src/main/java/me/spider/contestbot/structures/Submission.java
@@ -1,5 +1,18 @@
 package me.spider.contestbot.structures;
 
-public class Submission {
+import lombok.Data;
+import net.dv8tion.jda.core.entities.Message;
+
+public @Data
+class Submission {
+    private String submissionID;
+    private String submitterID;
+    private Message submissionContent;
+
+    public Submission(Message submissionContent){
+        this.submissionContent = submissionContent;
+        this.submissionID = submissionContent.getId();
+        this.submitterID = submissionContent.getAuthor().getId();
+    }
 
 }
