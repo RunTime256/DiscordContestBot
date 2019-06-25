eb6a87c52f63770d9c995bd46e0bd20600071c2b
RunTime256 <35320040+RunTime256@users.noreply.github.com>
2019-06-24 19:06:16 -0500

    Added MyBatis functionality

--- src/main/java/me/spider/contestbot/ContestBot.java
+++ src/main/java/me/spider/contestbot/ContestBot.java
@@ -1,7 +1,22 @@
 package me.spider.contestbot;
 
-public class ContestBot {
-    public static void main(String[] args) {
+import me.spider.contestbot.sql.Session;
+import me.spider.contestbot.sql.SessionFactory;
 
+import java.io.IOException;
+
+public class ContestBot
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            SessionFactory.init("PostgreSQLConfig.xml");
+        }
+        catch (IOException e)
+        {
+            System.out.println("IOException on SQL init");
+            e.printStackTrace();
+        }
     }
 }
